3755857cb3e769cc167ecad93b46320159a152a9
Abdul Zia <52299063+abdulziaa@users.noreply.github.com>
2023-12-02 21:56:47 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Turn off React StrictMode for now, as react-aria (used by Plasmic)
   // has some troubles with it. See
   // https://github.com/adobe/react-spectrum/labels/strict%20mode
   reactStrictMode: false,
+
+  // Specify the directory where the export will be placed
+  // Adjust the value based on your project structure
+  // By default, it will be placed in the 'out' directory
+  distDir: 'out',
 };
 
 module.exports = nextConfig;
